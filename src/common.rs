840996381223b2f17df75c4d8657ba68d84bb26f
evdwath <evdwath@gmail.com>
2026-09-22 21:40:48 +0200

    0922

--- src/common.rs
+++ src/common.rs
@@ -2186,7 +2186,7 @@ pub fn create_symmetric_key_msg(their_pk_b: [u8; 32]) -> (Bytes, Bytes, secretbo
 
 #[inline]
 pub fn using_public_server() -> bool {
-    crate::get_custom_rendezvous_server(get_option("custom-rendezvous-server")).is_empty()
+    false
 }
 
 pub struct ThrottledInterval {
